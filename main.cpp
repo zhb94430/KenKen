4311c841376f7c97518205be509ebea84bac0f6e
Hongbin Zhang <zhan2385@umn.edu>
2020-01-30 11:33:50 -0700

    stuff

--- main.cpp
+++ main.cpp
@@ -5,8 +5,26 @@
 
 #include "z3++.h"
 
-void SolveWithZ3(std::string input);
+void SolveWithZ3();
+void SolveWithMiniSAT();
 
+void ParseInput();
+
+struct Slot
+{
+	int x;
+	int y;
+}
+
+struct Cage
+{
+	int cageTarget;
+	std::string cageOperator;
+	std::vector<Slot> slotList;
+}
+
+std::vector<Cage> cageList;
+std::string inputAddress;
 
 int main(int argc, char const *argv[])
 {
@@ -18,32 +36,65 @@ int main(int argc, char const *argv[])
 		return 0;
 	}
 
-	std::string inputAddress = argv[1];
+	inputAddress = argv[1];
 
-	// Convert input into Z3 format
-	SolveWithZ3(inputAddress)
+	ParseInput();
 
-	GenerateMiniSATInput();
-	//Solve with minisat
-	system("./minisat ./minisatinput.txt ./minisatoutput.txt");
-	std::string outputAddress = "./minisatoutput.txt";
-	ProcessMiniSATOutput(outputAddress);
+	SolveWithZ3();
 
+	SolveWithMiniSAT();
 
 	return 0;
 }
 
 
-void SolveWithZ3(std::string input)
+void SolveWithZ3()
 {
-	FILE* inputFP = fopen(input);
+	z3::context c;
+	z3::solver s(c);
 
-	// Parse the input
-	
-
-	// Encode the input
+	// Encode the cages
+	for (int i = 0; i < cageList.size(); ++i)
+	{
+		
+	}
 
 	// Run the solver
 
 	// Process the result
+}
+
+void SolveWithMiniSAT()
+{
+	GenerateMiniSATInput();
+	//Solve with minisat
+	system("./minisat ./minisatinput.txt ./minisatoutput.txt");
+	std::string outputAddress = "./minisatoutput.txt";
+	ProcessMiniSATOutput(outputAddress);
+}
+
+void ParseInput()
+{
+	FILE* inputFP = fopen(input);
+	
+	while (inputFP != eof)
+	{
+		// Parse each line into a Cage
+		currentLine = ?;
+
+		Cage currentCage;
+
+		currentCage.cageTarget = currentLine[0];
+		currentCage.cageOperator = currentLine[1];
+
+		for (int i = 0; i < currentLine.size(); i=i+2)
+		{
+			Slot currentSlot;
+
+			currentSlot.x = atoi(currentLine[i]);
+			currentSlot.y = atio(currentLine[i+1]);
+
+			currentCage.slotList.push_back(currentSlot);
+		}
+	}
 }
